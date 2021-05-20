907611b95c6077fcf804f382f50891b27f11ed0d
COBO <ifaezcobo@gmail.com>
2021-05-20 13:41:02 -0500

    Initial commit

---
+++ app/src/main/java/com/isucorp/acmecompanytest/ui/MainActivity.java
@@ -0,0 +1,44 @@
+package com.isucorp.acmecompanytest.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.isucorp.acmecompanytest.R;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.action_add_item:
+            {
+                Toast.makeText(this, "TODO", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
